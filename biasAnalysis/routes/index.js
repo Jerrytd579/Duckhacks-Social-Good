1ebf7bf19cbe003a0246a6a308b8035e7d437e6e
Chaeli Vieira <cvieira@stevens.edu>
2020-08-13 16:17:45 -0400

    basic layout of api

---
+++ biasAnalysis/routes/index.js
@@ -0,0 +1,13 @@
+const indexRoutes = require('./form');
+const resultRoutes = require('./result');
+const path = require('path');
+
+const constructorMethod = (app) => {
+	app.use('/', indexRoutes);
+	app.use('/result', resultRoutes);
+	app.use('*', (req, res) => {
+		res.redirect('/');
+	});
+};
+
+module.exports = constructorMethod;
