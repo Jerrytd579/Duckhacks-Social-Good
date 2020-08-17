6b0eb08ba5e0a41cd9950e57dbf6c6deb8608ce4
adapap <19696846+adapap@users.noreply.github.com>
2020-08-17 17:29:08 -0400

    Add GCloud AutoML integration

--- dist/data/bias-analysis.js
+++ dist/data/bias-analysis.js
@@ -1,28 +1,26 @@
-import { PredictionServiceClient } from '@google-cloud/automl';
+const { PredictionServiceClient } = require('@google-cloud/automl').v1;
 
 const client = new PredictionServiceClient();
+const projectId = 'algebraic-depot-286523';
+const location = 'us-central1';
+const modelId = 'TST4181277793687961600';
 
-export default async function predict() {
-  // Construct request
-  const request = {
-    name: client.modelPath(projectId, location, modelId),
-    payload: {
-      textSnippet: {
-        content: content,
-        mimeType: 'text/plain', // Types: 'test/plain', 'text/html'
+module.exports = {
+  async predict(content) {
+    // Construct request
+    const request = {
+      name: client.modelPath(projectId, location, modelId),
+      payload: {
+        textSnippet: {
+          content: content,
+          mimeType: 'text/plain', // Types: 'test/plain', 'text/html'
+        },
       },
-    },
-  };
+    };
 
-  const [response] = await client.predict(request);
-  console.log();
-
-  for (const annotationPayload of response.payload) {
-    console.log(`Predicted class name: ${annotationPayload.displayName}`);
-    console.log(
-      `Predicted sentiment score: ${annotationPayload.textSentiment.sentiment}`
-    );
-  }
-}
-
-predict();
+    const [res] = await client.predict(request);
+    let predict = res.payload[0].textSentiment.sentiment;
+    let score = res.metadata.sentiment_score;
+    return predict, score;
+  },
+};
--- dist/routes/result.js
+++ dist/routes/result.js
@@ -2,27 +2,28 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data/bias-analysis');
 
-router.post('/', async (req, res) => {
-    const formData = req.body;
+process.env.GOOGLE_APPLICATION_CREDENTIALS = 'data/gauth.json';
 
-    if (!formData['text-to-test']) {
-        res.status(400);
-        res.render('result/result', {
-          title:"Bias Analyzer Results Error",
-          errors: true
-      });
-      return;
-    }
-    try {
-      const results = await data.check(formData['text-to-test']);
-      res.render('result/result', {
-        title:"Bias Analysis Results",
-        text : formData['text-to-test'],
-        analysis: results 
-      });
-      
-    } catch (e) {
-      res.status(500).json({error: e});
-    }
-  });
-  module.exports = router;
+router.post('/', async (req, res) => {
+  const formData = req.body;
+
+  if (!formData['text-to-test']) {
+    res.status(400);
+    res.render('result/result', {
+      title: 'Bias Analyzer Results Error',
+      errors: true,
+    });
+    return;
+  }
+  try {
+    const results = await data.predict(formData['text-to-test']);
+    res.render('result/result', {
+      title: 'Bias Analysis Results',
+      text: formData['text-to-test'],
+      analysis: results,
+    });
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+module.exports = router;
